963242c3026c9297b86d6f0fa621135cf486ac04
sobbey99 <sobbey99@gmail.com>
2023-06-25 13:42:53 +0300

    display section

---
+++ src/components/DisplaySection.jsx
@@ -0,0 +1,15 @@
+const DisplaySection = () => {
+  return (
+    <div className="display-section wrapper">
+      <h2 className="title">New</h2>
+      <p className="text">Briliant</p>
+      <span className="description">
+        A display that's up to 2x brighter in the sun.
+      </span>
+      <button className="button">Try me!</button>
+      <button className="back-button">TOP</button>
+    </div>
+  );
+};
+
+export default DisplaySection;
