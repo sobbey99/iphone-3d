84d55ca76d994e485624438e46f520f170674865
sobbey99 <sobbey99@gmail.com>
2023-06-25 14:51:00 +0300

    Loader

---
+++ src/components/Loader.jsx
@@ -0,0 +1,11 @@
+import AnimatedLogo from "../assets/images/logo-animated.gif";
+
+const Loader = () => {
+  return (
+    <div className="loader">
+      <img src={AnimatedLogo} alt="apple loader" className="logo" />
+    </div>
+  );
+};
+
+export default Loader;
